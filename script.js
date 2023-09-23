de44b9bc47d3662bae19d1dd0883ad94fb72be7b
Moussa Aminata Gueye <kalamouu@gmail.com>
2023-09-23 16:48:31 +0000

    Create the basic calculator style Defined the clicks function for querying the different number and made the basic operations

---
+++ script.js
@@ -0,0 +1,113 @@
+const display = document.getElementById('display')
+const digits = document.querySelectorAll('.digit')
+const resetButton = document.getElementById('resetButton')
+const deleteButton = document.getElementById('deleteButton')
+const operators = document.querySelectorAll('.operator')
+const equalButton = document.getElementById('equalButton')
+const decimalButton = document.getElementById('decimalButton')
+const signToggle = document.getElementById('signToggle')
+let firstOperand = ''
+let secondOperand = ''
+let resetScreen = false;
+let currentOperator = NaN;
+
+function appendDigit(number) {
+    if (display.textContent === '0')
+        resetDisplay();
+    display.textContent += number;
+}
+
+digits.forEach((digit) => digit.addEventListener('click', () => appendDigit(digit.textContent)))
+resetButton.addEventListener('click', () => clearDisplay())
+deleteButton.addEventListener('click', () => erase())
+operators.forEach((operator) => operator.addEventListener('click', ()=> {
+    firstOperand = display.textContent
+    currentOperator = operator.textContent
+    resetDisplay()
+}))
+
+equalButton.addEventListener('click', ()=> calculate())
+decimalButton.addEventListener('click', ()=> {
+    if (display.textContent.includes('.')){
+        return
+    }
+    if (display.textContent === '0') {
+        display.textContent = '0'
+    }
+    appendDigit('.')
+})
+
+signToggle.addEventListener('click', ()=> {
+    display.textContent = negate(display.textContent)
+})
+
+function clearDisplay() {
+    display.textContent = '0'
+    currentOperator = null
+}
+
+function resetDisplay() {
+    display.textContent = '';
+}
+
+function erase() {
+    display.textContent = display.textContent.slice(0,-1)
+    if (currentOperator !== null && display.textContent !== '0') {
+        currentOperator = null
+    }
+    if (display.textContent.length === 0 && display.textContent !== 0) {
+        display.textContent = '0'
+    }
+}
+
+function calculate() {
+    if (currentOperator !== null) {
+        secondOperand = display.textContent;
+        const result = operate(currentOperator, firstOperand, secondOperand);
+        display.textContent = round(result);
+    }
+}
+
+/* operations */
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function percentage(a) {
+    return a / 100;
+}
+
+function negate(a) {
+    return -1 * a;
+}
+
+function round(a) {
+    return Math.round(1000*a) / 1000
+}
+
+function operate(operand, a, b) {
+    a = Number(a);
+    b = Number(b);
+    switch (operand) {
+        case "+":
+            return add(a, b)
+        case "-":
+            return subtract(a, b)
+        case "×":
+            return multiply(a, b)
+        case "÷":
+            return divide(a, b)
+    }
+}
